ee48287ff781f30ded8cbf9c55eaa5f638f1bf9d
hughcrt <me@hugh.sh>
2021-03-09 12:41:00 +0900

    Add neuron landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,9 +14,9 @@ export default function Home() {
         <h1 className={styles.title}>Hugh Chocart</h1>
 
         <p className={styles.description}>
-          I'm a software engineer consultant. 
+          I'm a software engineer consultant.
           <br />I founded{" "}
-          <a href="https://neuron." target="_blank">
+          <a href="https://neuron.hugh.sh" target="_blank">
             neurON
           </a>
           , which I manage during my spare time.
@@ -24,12 +24,19 @@ export default function Home() {
           Currently based in Seoul 🇰🇷
         </p>
       </main>
-       <SocialProofSection />
+      <SocialProofSection />
 
       <footer className={styles.footer}>
         <div></div>
         <a href="mailto:me@hugh.sh">me@hugh.sh</a>
-        <a className="github" href="https://github.com/hughcrt" target="_blank"><Image src="/img/github.svg" alt="Github Logo" width={30} height={30} /></a>
+        <a className="github" href="https://github.com/hughcrt" target="_blank">
+          <Image
+            src="/img/github.svg"
+            alt="Github Logo"
+            width={30}
+            height={30}
+          />
+        </a>
       </footer>
     </div>
   );
